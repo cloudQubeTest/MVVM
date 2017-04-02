d4325e6f932efee5fc65f8028679db2ac708a415
Colin Brinton <brintonc@seattleu.edu>
2017-04-01 18:22:27 -0700

    initial baseline

---
+++ TestWPF/Contact/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using PatientMVVM.Models;
+
+namespace PatientMVVM.ViewModels
+{
+    public class ContactViewModel : ObservableObject
+    {
+        private string _someText;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ TestWPF/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using PatientMVVM.Models;
+
+namespace PatientMVVM.ViewModels
+{
+    public class Presenter : ObservableObject
+    {
+        private string _someText;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = "Testing";
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+  
+
+ 
+    }
+}
