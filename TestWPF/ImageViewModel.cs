439f10d86d38687bb3252f0862a7eb5a0b3d52ff
Colin Brinton <brintonc@seattleu.edu>
2017-04-22 16:59:29 -0700

    Add image button working

--- TestWPF/ImageViewModel.cs
+++ TestWPF/ImageViewModel.cs
@@ -5,6 +5,8 @@ using System.Windows.Media.Imaging;
 using System.Windows;
 using PatientsDataModel;
 using System.IO;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace PatientMVVM
 {
@@ -19,11 +21,31 @@ namespace PatientMVVM
                           BitmapSizeOptions.FromEmptyOptions());
         }
 
+        public static Bitmap BitmapSourceToBitmap(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
         public static System.Drawing.Image byteArrayToImage(byte[] byteArrayIn)
         {
             MemoryStream ms = new MemoryStream(byteArrayIn);
             System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
             return returnImage;
+        }
+
+        public static byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
         }
     }
     public class ImageViewModel : ObservableObject
@@ -79,8 +101,57 @@ namespace PatientMVVM
 
         public void updateImage()
         {
-            Bitmap bitmap = new Bitmap(ImageConversion.byteArrayToImage(SelectedPatient.Image));
-            ImageSource = ImageConversion.BitmapToBitmapSource(bitmap);
+            if (SelectedPatient.Image != null)
+            {
+                Bitmap bitmap = new Bitmap(ImageConversion.byteArrayToImage(SelectedPatient.Image));
+                ImageSource = ImageConversion.BitmapToBitmapSource(bitmap);
+            }
+        }
+
+        private string getFilePath()
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            string path = "";
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                path = dialog.FileName;
+            }
+            return path;
+        }
+
+
+        private System.Drawing.Image imgToStore;
+
+        private void AddImageClick()
+        {
+            string path = getFilePath();
+            Bitmap addImg = new Bitmap(path);
+            ImageSource = ImageConversion.BitmapToBitmapSource(addImg);
+
+            imgToStore = System.Drawing.Image.FromFile(path);
+            SelectedPatient.Image = ImageConversion.imageToByteArray(imgToStore);
+            //Console.Write(path);
+        }
+
+        public ICommand AddClickCommand
+        {
+            get { return new DelegateCommand(AddImageClick); }
+        }
+
+
+        private void GrayscaleClick()
+        {
+            //Bitmap bpm = new Bitmap(ImageConversion.BitmapSourceToBitmap(_bitmapSource));
+            FormatConvertedBitmap grayscaleBpm = new FormatConvertedBitmap(_bitmapSource, PixelFormats.Gray8, BitmapPalettes.Gray256, 0.0);
+            ImageSource = grayscaleBpm;
+            //SelectedPatient.Image = ImageConversion.imageToByteArray(ImageConversion.BitmapSourceToBitmap(ImageSource));
+
+        }
+
+        public ICommand GrayscaleClickCommand
+        {
+            get { return new DelegateCommand(GrayscaleClick); }
         }
 
     }
